ad58d188dbc9a786c4105aee9dd2927838ff2921
jamesdawson <jdaw2591@uni.sydney.edu.au>
2023-10-23 23:48:50 +1100

    Added code for BFS

--- major_project/include/major_project/unexplored_nav.h
+++ major_project/include/major_project/unexplored_nav.h
@@ -1,6 +1,8 @@
 #ifndef UNEXPLORED_NAV
 #define UNEXPLORED_NAV
 
+#include <utility>
+#include <queue>
 
 /**
  * Unexplored navigation class
@@ -9,6 +11,25 @@
 class CUnexploredNav 
 {
   public:
+    CUnexploredNav();
+
   private:
+    // Will probably need to change this
+    static const int ROW = 5;
+    static const int COL = 5;
+
+    // Will probably need to change the inputs to match with smart pointer
+    // (object_name->data.at(10,20))
+    bool isValid(bool vis[][COL], int row, int col);
+    void BFS(int grid[][COL], int costmap[][COL], bool vis[][COL], int row, int col);
+
+    // Array for recording visited pixels
+    bool visited[ROW][COL];
+
+    // Vector of pairs for storing the boundary pixels
+    std::vector<std::pair<int, int>> boundaryPixels;
+
+    
 };
+
 #endif // UNEXPLORED_NAV
--- major_project/src/unexplored_nav.cpp
+++ major_project/src/unexplored_nav.cpp
@@ -1,2 +1,98 @@
 #include "../include/major_project/unexplored_nav.h"
+#include <utility>
+#include <queue>
+#include <iostream>
 
+
+/*
+1. Use the regular occupancy grid (/map) to find pixels that are on the boundary
+	of unexplored. These
+
+2. Use /move_base global cost map to remove any points that are red (close to a
+	nearby wall)
+*/
+
+// Define constants
+// Direction arrays are used to traverse the adjacent cells
+const int dRow[] = {-1, 0, 1, 0};
+const int dCol[] = {0, 1, 0, -1};
+
+/*
+Smart pointer data
+
+*/
+
+// Constructor
+CUnexploredNav::CUnexploredNav() {
+    //
+}
+
+
+// Need a function to check if a cell has been visited already
+bool CUnexploredNav::isValid(bool vis[][COL], int row, int col) {
+    bool check;
+
+    // If cell lies out of bounds
+    if (row < 0 || col < 0 || row >= ROW || col >= COL) {
+        check = false;
+    }
+    // If cell is already visited
+    else if (vis[row][col]) {
+        check = false;
+    }
+    // Otherwise true
+    else {
+        check = true;
+    }
+ 
+    // Otherwise
+    return check;
+}
+
+// Need a function to perform the BFS algorithm
+void CUnexploredNav::BFS(int grid[][COL], int costmap[][COL], bool vis[][COL], int row, int col)
+{
+    // Stores indices of the matrix cells
+    std::queue<std::pair<int, int>> q;
+ 
+    // Mark the starting cell as visited and push it into the queue
+    q.push({ row, col });
+    visited[row][col] = true;
+ 
+    // Iterate while the queue is not empty
+    while (!q.empty()) {
+ 
+        std::pair<int, int> cell = q.front();
+        int x = cell.first;
+        int y = cell.second;
+ 
+        std::cout << grid[x][y] << " ";
+ 
+        q.pop();
+ 
+        // Search the adjacent cells (up, down, left, right)
+        bool isBoundary = false;
+        for (int i = 0; i < 4; i++) {
+ 
+            int adjx = x + dRow[i];
+            int adjy = y + dCol[i];
+
+            // If the adjacent pixel is not visited before
+            if (isValid(visited, adjx, adjy)) {
+                q.push({ adjx, adjy });
+                vis[adjx][adjy] = true;
+
+                // Logic for if pixels are on the boundary of unexplored regions
+                // If adjacent pixel is unexplored and is far away enough from
+                // any nearby walls, then add pixel to vector of pairs
+                if (grid[adjx][adjy] == PLACEHOLDER && costmap[adjx][adjy] < PLACEHOLDER && isBoundary == false) {
+                    // Add pixel by pushing back onto the vector
+                    boundaryPixels.push_back(std::make_pair(x, y));
+
+                    // Check if at least one adjacent pixel is unxeplored
+                    isBoundary = true;
+                }
+            }
+        }
+    }
+}
