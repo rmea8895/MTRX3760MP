6938a002cf99afe6e1cc41aeb8174222fce0bcfc
Monkey <rmea8895@uni.sydney.edu.au>
2023-10-24 14:07:24 +1100

    Modified map subscribers, now compiles

--- major_project/include/major_project/maps.h
+++ major_project/include/major_project/maps.h
@@ -3,24 +3,38 @@
 
 #include "ros/ros.h"
 #include <nav_msgs/OccupancyGrid.h> 
+#include <utility>
+#include <vector>
+
+//#define MAP_DEBUG
 
 /**
  * Class that subscribes to /map and 
  * /move_base/global_costmap/costmap
+ * note both of these maps are the global maps
 */
 
 class CMaps
 {
   public:
-    CMaps(ros::NodeHandle *nh_);
+    CMaps(ros::NodeHandlePtr nh_);
     ~CMaps();
+
+
+    // Returns pointer to map
+    auto getMapPtr();
+    // Returns pointer to cost map
+    auto getCostMapPtr();
+    /// Signed char => 8 bits
+    std::vector<std::vector<signed char>> map;
+    std::vector<std::vector<signed char>> costMap;
 
   private:
     /// Subscriber node handlers
     ros::Subscriber map_sub_;
     ros::Subscriber costMap_sub_;
 
-    bool init(ros::NodeHandle *nh_);
+    bool init(ros::NodeHandlePtr nh_);
     
     void mapCallBack(const nav_msgs::OccupancyGrid::ConstPtr& msg);
     void costMapCallBack(const nav_msgs::OccupancyGrid::ConstPtr& msg);
--- major_project/src/maps.cpp
+++ major_project/src/maps.cpp
@@ -1,16 +1,16 @@
 #include "../include/major_project/maps.h"
 
-CMaps::CMaps(ros::NodeHandle *nh_)
+CMaps::CMaps(ros::NodeHandlePtr nh_)
 {
   ROS_INFO("Map subscribers init");
-  bool ret = init(ros::NodeHandle *nh_);
+  bool ret = init(nh_);
   // check initialization
   ROS_ASSERT(ret);
 }
 
-bool CMaps::init(ros::NodeHandle *nh_)
+bool CMaps::init(ros::NodeHandlePtr nh_)
 {
-  map_sub__ = nh_->subscribe("/map", 2, &CMaps::mapCallBack, this);
+  map_sub_ = nh_->subscribe("/map", 2, &CMaps::mapCallBack, this);
   costMap_sub_ = nh_->subscribe("/move_base/global_costmap/costmap", 2, &CMaps::costMapCallBack, this);
   return true;
 }
@@ -19,11 +19,47 @@ void CMaps::mapCallBack(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
   // cell size is default 0.05m/cell or 5cm per cell resolution
   // probably want 2cm per cell res, can modify this in the yaml config
-  
+  unsigned int width = msg->info.width;
+  unsigned int height = msg->info.height;
+
+  for(int i = 0; i < width; i++)
+  {
+    for(int j = 0; j < height; j++)
+    {
+      map[i][j] = msg->data[i+j*width];
+    }
+  }
+  #ifdef MAP_DEBUG
+    ROS_INFO("State [%d]", msg);
+  #endif
 }
 
 void CMaps::costMapCallBack(const nav_msgs::OccupancyGrid::ConstPtr& msg)
 {
-  
+  unsigned int width = msg->info.width;
+  unsigned int height = msg->info.height;
 
+  for(int i = 0; i < width; i++)
+  {
+    for(int j = 0; j < height; j++)
+    {
+      costMap[i][j] = msg->data[i+j*width];
+    }
+  }
+}
+
+auto CMaps::getMapPtr()
+{
+  auto mapPtr = &map;
+  return mapPtr;
+}
+
+auto CMaps::getCostMapPtr()
+{
+  auto costMapPtr = &costMap;
+  return costMapPtr;
+}
+
+CMaps::~CMaps()
+{
 }
